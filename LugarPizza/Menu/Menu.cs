34378789e52105c36f5b9b4591d2309626b8f68f
darcklito <luis_paulino5@ucne.edu.do>
2023-09-25 18:07:24 -0400

    Menu de pizzas (lista) agregado

---
+++ LugarPizza/Menu/Menu.cs
@@ -0,0 +1,14 @@
+﻿using LugarPizza.Models;
+
+namespace LugarPizza.Menu
+{
+    public class Menu
+    {
+        public List<Pizzas> Pizzas { get; set; }
+        = new List<Pizzas>();
+        public void Add(Pizzas pizza)
+        => Pizzas.Add(pizza);
+        public Pizzas? GetPizza(int id)
+        => Pizzas.SingleOrDefault(pizza => pizza.Id == id);
+    }
+}
