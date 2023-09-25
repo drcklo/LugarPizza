16f4798bde8112e441f46c86eb5743a248cf3fbf
darcklito <luis_paulino5@ucne.edu.do>
2023-09-25 17:47:38 -0400

    Modelo de la pizza creado

---
+++ LugarPizza/Models/Pizzas.cs
@@ -0,0 +1,18 @@
+﻿namespace LugarPizza.Models
+{
+    public class Pizzas
+    {
+        public Pizzas(int id, string nombre, decimal precio,
+        NivelesDePicante nivelDePicante)
+        {
+            this.Id = id;
+            this.Nombre = nombre;
+            this.Precio = precio;
+            this.NivelesDePicante = nivelDePicante;
+        }
+        public int Id { get; }
+        public string Nombre { get; }
+        public decimal Precio { get; }
+        public NivelesDePicante NivelesDePicante { get; }
+    }
+}
