000f6553b7662bfa2b8f8f8b3cf3237f1e9d1b59
darcklito <luis_paulino5@ucne.edu.do>
2023-10-10 13:39:33 -0400

    Clase de DebuggExtensions agregada

---
+++ LugarPizza/Clients/ExtensionesDeDebug.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+namespace LugarPizza.Clients
+{
+	public static class ExtensionesDeDebug
+	{
+		private static JsonSerializerOptions opciones = new
+	   JsonSerializerOptions
+		{ WriteIndented = true };
+		public static string ToJson(this object obj)
+		=> JsonSerializer.Serialize(obj, opciones);
+	}
+
+}
