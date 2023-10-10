1959cd80fdf73c5ac14684502ff90b68b01aaa6e
darcklito <luis_paulino5@ucne.edu.do>
2023-10-10 13:14:10 -0400

    Metodo agregar actualizado

--- LugarPizza/DAL/Menu.cs
+++ LugarPizza/DAL/Menu.cs
@@ -6,7 +6,7 @@ namespace LugarPizza.DAL
     {
         public List<Pizzas> ListaDePizzas { get; set; }
         = new List<Pizzas>();
-        public void Add(Pizzas pizza)
+        public void AgregarPizza(Pizzas pizza)
         => ListaDePizzas.Add(pizza);
         public Pizzas? GetPizza(int id)
         => ListaDePizzas.SingleOrDefault(pizza => pizza.Id == id);
