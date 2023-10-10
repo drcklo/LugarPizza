9f9b10cc6fda0908068a7a98ca8aaecd744d1b58
darcklito <luis_paulino5@ucne.edu.do>
2023-10-10 11:43:19 -0400

    Funcion de agregar añadida al CarritoDeCompra

--- LugarPizza/Models/CarritoDeCompra.cs
+++ LugarPizza/Models/CarritoDeCompra.cs
@@ -5,5 +5,8 @@
         public Clientes Clientes { get; set; } = new Clientes();
         public List<int> Ordenes { get; set; } = new List<int>();
         public bool Pagado { get; set; }
-    }
+        public void Agregar(int pizzaId)
+        => Ordenes.Add(pizzaId);
+
+	}
 }
