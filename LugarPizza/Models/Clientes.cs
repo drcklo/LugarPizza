e846a468681771a5de8791c4dd192363f0515850
darcklito <luis_paulino5@ucne.edu.do>
2023-10-10 13:43:59 -0400

    Parametros [Required] agregados a Clientes

--- LugarPizza/Models/Clientes.cs
+++ LugarPizza/Models/Clientes.cs
@@ -1,10 +1,16 @@
-﻿namespace LugarPizza.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LugarPizza.Models
 {
     public class Clientes
     {
+       
         public int IdCliente { get; set; }
-        public string Nombre { get; set; } = default!;
-        public string Calle { get; set; } = default!;
-        public string Ciudad { get; set; } = default!;
+		[Required(ErrorMessage = "Por favor escriba un nombre")]
+		public string Nombre { get; set; } = default!;
+		[Required(ErrorMessage = "Por favor escriba una calle con su numero de casa")]
+		public string Calle { get; set; } = default!;
+		[Required(ErrorMessage = "Por favor escriba una ciudad")]
+		public string Ciudad { get; set; } = default!;
     }
 }
