ffc91ce92c50512e77b7b356ff86510aee5ca23e
darcklito <luis_paulino5@ucne.edu.do>
2023-10-10 10:40:11 -0400

    Creacion de modelo de clientes

---
+++ LugarPizza/Models/Clientes.cs
@@ -0,0 +1,10 @@
+﻿namespace LugarPizza.Models
+{
+    public class Clientes
+    {
+        public int IdCliente { get; set; }
+        public string Nombre { get; set; } = default!;
+        public string Calle { get; set; } = default!;
+        public string Ciudad { get; set; } = default!;
+    }
+}
