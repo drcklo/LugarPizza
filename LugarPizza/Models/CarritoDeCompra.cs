fed37ba82a7bc685750a7feb5499c0d068ba243e
darcklito <luis_paulino5@ucne.edu.do>
2023-10-10 10:50:37 -0400

    modelo de carrito de compra creado

---
+++ LugarPizza/Models/CarritoDeCompra.cs
@@ -0,0 +1,9 @@
+﻿namespace LugarPizza.Models
+{
+    public class CarritoDeCompra
+    {
+        public Clientes Clientes { get; set; } = new Clientes();
+        public List<int> Ordenes { get; set; } = new List<int>();
+        public bool Pagado { get; set; }
+    }
+}
