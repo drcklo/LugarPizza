55ed1774b5ee1f1fedec3db18214e391db7ecd74
darcklito <luis_paulino5@ucne.edu.do>
2023-10-10 11:01:22 -0400

    Modelo de estado implementado

---
+++ LugarPizza/Models/Estados.cs
@@ -0,0 +1,11 @@
+﻿using LugarPizza.DAL;
+
+namespace LugarPizza.Models
+{
+    public class Estados
+    {
+        public Menu Menu { get; } = new Menu();
+        public CarritoDeCompra Carrito { get; } = new CarritoDeCompra();
+        public InterfazDeUsuario InterfazDeUsuario { get; set; } = new InterfazDeUsuario();
+    }
+}
